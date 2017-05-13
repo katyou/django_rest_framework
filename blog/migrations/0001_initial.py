00a8ffcee4e7ae55f3c0013cdecc7a5c6c64633c
katyou <ienaga@ijgn.jp>
2017-05-13 11:08:08 +0900

    initial commit

---
+++ blog/migrations/0001_initial.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Entry',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('title', models.CharField(max_length=128)),
+                ('body', models.TextField()),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('status', models.CharField(max_length=8, default='draft', choices=[('draft', '下書き'), ('public', '公開中')])),
+            ],
+        ),
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('name', models.CharField(max_length=32)),
+                ('mail', models.EmailField(max_length=254)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='entry',
+            name='author',
+            field=models.ForeignKey(related_name='entries', to='blog.User'),
+        ),
+    ]
